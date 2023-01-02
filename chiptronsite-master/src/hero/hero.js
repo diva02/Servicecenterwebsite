fef9bd9e85a2b66539132f7a3c5544713054ac8b
diva02 <divagarselvakumar@gmail.com>
2023-01-02 14:14:38 +0530

    commit

--- chiptronsite-master/src/hero/hero.js
+++ chiptronsite-master/src/hero/hero.js
@@ -34,7 +34,7 @@ export default function Hero() {
                         <img className="mr-2 lg:mr-12 mt-2 lg:mt-12 absolute right-0 top-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg2.svg" alt="bg" />
                         <img className="ml-2 lg:ml-12 mb-2 lg:mb-12 absolute bottom-0 left-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg3.svg" alt="bg" />
                         <div className="w-11/12 sm:w-2/3 mb-5 sm:mb-7">
-                            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-white font-bold leading-tight ">We're here for you whenever you need us</h1>
+                            <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center flex text-white font-bold leading-tight ">We're here for you whenever you need us</h1>
                         </div>
                         {/* <div className="flex justify-center items-center mb-10 sm:mb-20">
                             <button className="hover:text-white hover:bg-transparent lg:text-xl hover:border-white border bg-white transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700	focus:ring-white rounded text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Get Started</button>
