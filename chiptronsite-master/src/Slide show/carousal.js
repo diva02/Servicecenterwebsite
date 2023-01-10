6ce0d52b724ad779faac8b601233eddd3137a912
diva02 <divagarselvakumar@gmail.com>
2023-01-10 20:40:53 +0530

    c

--- chiptronsite-master/src/Slide show/carousal.js
+++ chiptronsite-master/src/Slide show/carousal.js
@@ -1,25 +1,25 @@
 import ImageSlider, { Slide } from "react-auto-image-slider";
 export default function Caro() {
   return (
-    <div className="w-full  items-center flex">
+    <div className="w-full sm:  items-center flex">
     <ImageSlider  effectDelay={200} autoPlayDelay={2500}>
       <Slide>
-        <img alt="img2" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/desktop.jpg" />
+        <img alt="img2" className="sm:w-auto sm:h-auto sm:object-center" src="http://www.chiptronix.in/img/desktop.jpg" />
       </Slide>
       <Slide>
-        <img alt="img2"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/chipp.jpg" />
+        <img alt="img2"  className="sm:w-auto sm:h-auto sm:object-center" src="http://www.chiptronix.in/img/chipp.jpg" />
       </Slide>
       <Slide>
-        <img alt="img1"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/eee.png" />
+        <img alt="img1"  className="sm:w-auto sm:object-center sm:h-auto" src="http://www.chiptronix.in/img/eee.png" />
       </Slide>
       <Slide>
-        <img alt="img2"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/laptop.jpg" />
+        <img alt="img2"  className="sm:w-auto sm:object-center sm:h-auto" src="http://www.chiptronix.in/img/laptop.jpg" />
       </Slide>
       <Slide>
-        <img alt="img2" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/start.jpg" />
+        <img alt="img2" className="sm:w-auto sm:object-center sm:h-auto" src="http://www.chiptronix.in/img/start.jpg" />
       </Slide>
       <Slide>
-        <img alt="img1" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/topservice.png" />
+        <img alt="img1" className="sm:w-auto sm:object-center sm:h-auto" src="http://www.chiptronix.in/img/topservice.png" />
       </Slide>
     </ImageSlider>
   
--- chiptronsite-master/src/hero/hero.js
+++ chiptronsite-master/src/hero/hero.js
@@ -40,15 +40,15 @@ export default function Hero() {
                     <div className="mt-0 relative rounded-lg bg-indigo-700 w-fill flex flex-col items-center pt-0 sm:pt-24 pb-20 sm:pb-24 md:pb-48 lg:pb-56 xl:pb-64">
                         <img className="mr-2 lg:mr-12 mt-2 lg:mt-12 absolute right-0 top-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg2.svg" alt="bg" />
                         <img className="ml-2 lg:ml-12 mb-2 lg:mb-12 absolute bottom-0 left-0" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/center_aligned_with_image-svg3.svg" alt="bg" />
-                        <div className="sm:pt-2 w-11/12 sm:w-2/3 mb-5 items-center">
-                            <h1 className="text-3xl  sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center  flex text-white font-bold leading-relaxed ">We're here for you whenever you need us</h1>
+                        <div className="sm:pt-6 w-11/12 sm:w-2/3 mb-5 items-center">
+                            <h1 className="text-3xl pt-5  sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center  flex text-white font-bold leading-relaxed ">We're here for you whenever you need us</h1>
                         </div>
                         {/* <div className="flex justify-center items-center mb-10 sm:mb-20">
                             <button className="hover:text-white hover:bg-transparent lg:text-xl hover:border-white border bg-white transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700	focus:ring-white rounded text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Get Started</button>
                             <button className="hover:bg-white hover:text-indigo-600 lg:text-xl hover:border-indigo-600 ml-3 sm:ml-6 bg-transparent transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 ring-offset-indigo-700 focus:ring-white hover:bg-indigo-700-800 rounded border border-white text-white px-4 sm:px-8 py-1 sm:py-3 text-sm">Learn More</button>
                         </div> */}
                     </div>
-                    <div className="container mx-auto flex justify-center md:-mt-60 mt-0 sm:mt-16">
+                    <div className="container mx-auto flex justify-center md:-mt-56 mt-0 sm:-mt-36">
                         <div className="relative sm:w-4/5 w-11/12 md:w-1/2">
                             {/* <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/hero/h_2.png" alt="Sample Page" role="img" /> */}
                             <Lottie animationData={Service} className=" pb-0 " />
