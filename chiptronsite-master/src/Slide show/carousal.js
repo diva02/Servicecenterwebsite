0394f8f6b2e2aa49ad1b2fd1b9d15db5b188614e
diva02 <divagarselvakumar@gmail.com>
2023-01-10 20:32:17 +0530

    c

--- chiptronsite-master/src/Slide show/carousal.js
+++ chiptronsite-master/src/Slide show/carousal.js
@@ -4,22 +4,22 @@ export default function Caro() {
     <div className="w-full  items-center flex">
     <ImageSlider  effectDelay={200} autoPlayDelay={2500}>
       <Slide>
-        <img alt="img2" className="sm:w-full" src="http://www.chiptronix.in/img/desktop.jpg" />
+        <img alt="img2" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/desktop.jpg" />
       </Slide>
       <Slide>
-        <img alt="img2" src="http://www.chiptronix.in/img/chipp.jpg" />
+        <img alt="img2"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/chipp.jpg" />
       </Slide>
       <Slide>
-        <img alt="img1" src="http://www.chiptronix.in/img/eee.png" />
+        <img alt="img1"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/eee.png" />
       </Slide>
       <Slide>
-        <img alt="img2" src="http://www.chiptronix.in/img/laptop.jpg" />
+        <img alt="img2"  className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/laptop.jpg" />
       </Slide>
       <Slide>
-        <img alt="img2" src="http://www.chiptronix.in/img/start.jpg" />
+        <img alt="img2" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/start.jpg" />
       </Slide>
       <Slide>
-        <img alt="img1" src="http://www.chiptronix.in/img/topservice.png" />
+        <img alt="img1" className="sm:w-auto sm:h-auto" src="http://www.chiptronix.in/img/topservice.png" />
       </Slide>
     </ImageSlider>
   
