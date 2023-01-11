098dc0657c64f3b74045bc423001f64f31b996b1
diva02 <divagarselvakumar@gmail.com>
2023-01-11 21:20:21 +0530

    c

--- chiptronsite-master/src/Reservation/reservation.js
+++ chiptronsite-master/src/Reservation/reservation.js
@@ -13,31 +13,31 @@ function Reservation() {
                             <div className="flex flex-col  xl:px-0 px-4">
                             <h2 className="text-gray-800  md:text-lg font-bold xl:text-3xl leading-8 tracking-wider">Book a Service</h2>
 
-                                <div className="w-full grid grid-cols-2  2xl:w-8/12 mt-3">
-                                    <div className="mt-4 md:mt-8">
+                                <div className="w-full grid grid-cols-2 gap-4 2xl:w-8/12 mt-3">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800  text-2xl font-medium">Date</p>
-                                        <input className="mt-3 text-base border-2 w-11/12 lg:w-1/2 xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="Date" placeholder="date" />
+                                        <input className="mt-3 text-base border-2 w-full lg:w-full xl:w-full hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="Date" placeholder="date" />
                                     </div>
                                     
-                                    <div className="mt-4 md:mt-8">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800 text-2xl font-medium">Service</p>
-                                        <input className="mt-3 text-base border-2 w-11/12 lg:w-1/2 xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Laptop or Desktop" />
+                                        <input className="mt-3 text-base border-2 w-full lg:w-full xl:w-full hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Laptop or Desktop" />
                                     </div>
-                                    <div className="mt-4 md:mt-8">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800 text-2xl font-medium">Name</p>
-                                        <input className="mt-3 text-base border-2 w-11/12 lg:w-1/2 xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Ajay" />
+                                        <input className="mt-3 text-base border-2 w-full lg:w-full xl:w-full hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Ajay" />
                                     </div>
-                                    <div className="mt-4 md:mt-8">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800 text-2xl font-medium">Phone</p>
-                                        <input className="mt-3 text-base border-2 w-11/12 lg:w-1/2 xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Mobile number" />
+                                        <input className="mt-3 text-base border-2 w-full lg:w-full xl:w-full hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="text" placeholder="Mobile number" />
                                     </div>
-                                    <div className="mt-4 md:mt-8">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800 text-2xl font-medium">Email Address</p>
-                                        <input className="mt-3 text-base border-2 w-11/12 lg:w-1/2 xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="email" placeholder="example@mail.com" />
+                                        <input className="mt-3 text-base border-2 w-full lg:w-full xl:w-full hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800" type="email" placeholder="example@mail.com" />
                                     </div>
-                                    <div className="mt-4 md:mt-8">
+                                    <div className="mt-4 ">
                                         <p className="text-gray-800 text-2xl font-medium">Complaint</p>
-                                        <textarea className="mt-3 text-base border-2 w-11/12 lg:w-full xl:w-10/12 resize-none hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black xl:h-40 py-5 pl-4 text-gray-800" type="text" placeholder="problems..." defaultValue={""} />
+                                        <textarea className="mt-3 text-base border-2 w-full lg:w-full xl:w-full resize-none hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black xl:h-40 py-5 pl-4 text-gray-800" type="text" placeholder="problems..." defaultValue={""} />
                                     </div>
                                     <div className="py-5">
                                         <button className="py-3 md:py-5 px-5 md:px-10 bg-indigo-700 text-white hover:opacity-90 ease-in duration-150 text-sm md:text-lg tracking-wider font-semibold">Book Service</button>
